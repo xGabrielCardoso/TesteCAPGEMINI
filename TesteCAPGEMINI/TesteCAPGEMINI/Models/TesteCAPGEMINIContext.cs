71450110f5a335c4a68cd7dc20ef3221bb2b77d0
Gabriel Cardoso <gabriel_cardoso_98@hotmail.com>
2024-04-23 09:07:14 -0300

    Tela de Pedidos

--- TesteCAPGEMINI/TesteCAPGEMINI/Models/TesteCAPGEMINIContext.cs
+++ TesteCAPGEMINI/TesteCAPGEMINI/Models/TesteCAPGEMINIContext.cs
@@ -25,7 +25,7 @@ public partial class TesteCAPGEMINIContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=Gabriel-PC;Initial Catalog=TesteCAPGEMINI;User ID=sa;Password=123456;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=Gabriel-PC;Initial Catalog=TesteCAPGEMINI;User ID=sa;Password=123456");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
